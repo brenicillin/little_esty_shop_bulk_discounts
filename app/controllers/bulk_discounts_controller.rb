88bd6032d19f47395286b9a67f5b603c0cc088b0
Brandon Johnson <120131327+johnsonbw2010@users.noreply.github.com>
2023-04-22 12:13:43 -0400

    add show action to BD controller

--- app/controllers/bulk_discounts_controller.rb
+++ app/controllers/bulk_discounts_controller.rb
@@ -4,6 +4,10 @@ class BulkDiscountsController < ApplicationController
     @discounts = @merchant.bulk_discounts
   end
 
+  def show
+    @discount = BulkDiscount.find(params[:id])
+  end
+
   def new
     @merchant = Merchant.find(params[:merchant_id])
     @discount = BulkDiscount.new
