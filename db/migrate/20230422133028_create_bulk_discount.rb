97c1f7ef20d6c417f98e51cddf4118238a4508d5
Brandon Johnson <120131327+johnsonbw2010@users.noreply.github.com>
2023-04-22 10:27:05 -0400

    create bulk_discount migration, update schema

---
+++ db/migrate/20230422133028_create_bulk_discount.rb
@@ -0,0 +1,10 @@
+class CreateBulkDiscount < ActiveRecord::Migration[5.2]
+  def change
+    create_table :bulk_discounts do |t|
+      t.references :merchant, foreign_key: true
+      t.string :name
+      t.integer :threshold
+      t.integer :discount
+    end
+  end
+end
